227f86e9d186d3cec8501307a2ff96effb7669c4
Sandro Luiz Mazzolla Junior <jmazzolla94@gmail.com>
2024-06-27 19:08:02 -0300

    CONFIG: Implementando entities com sqlite

---
+++ src/models/sqlite/entities/people.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from src.models.sqlite.settings.base import Base
+
+class PetsTable(Base):
+    __tablename__ = "pets"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    pet_id = Column(Integer, ForeignKey("pets.id"), nullable=False)
+
+
+    def __repr__(self):
+        return f"<People(name={self.first_name}, last_name={self.last_name}, age={self.age}, pet_id={self.pet_id})>"
