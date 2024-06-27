b3aef7215c3fa53bff5de1ebe103ba47e8cc8265
Sandro Luiz Mazzolla Junior <jmazzolla94@gmail.com>
2024-06-27 19:43:37 -0300

    FEAT: Implementado método de listagem de pets e teste de integração

---
+++ src/models/sqlite/repositories/repositories_test.py
@@ -0,0 +1,16 @@
+
+import pytest
+from src.models.sqlite.settings.connection import db_connection_handler
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Interação com o banco de dados SQLite.")
+def test_list_pets():
+    """
+    Testando o método list_pets da classe PetsRepository.
+    """
+    pets_repository = PetsRepository(db_connection_handler)
+    pets = pets_repository.list_pets()
+
+    print(pets)
