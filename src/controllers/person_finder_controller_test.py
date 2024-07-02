12a3554d098f1a3846ef86b4814840a3a0bcdc55
Sandro Luiz Mazzolla Junior <jmazzolla94@gmail.com>
2024-07-02 18:50:11 -0300

    FEAT: Person Finder Controller & Teste Unitário na busca de person

---
+++ src/controllers/person_finder_controller_test.py
@@ -0,0 +1,37 @@
+#pylint: disable=unused-argument
+from .person_finder_controller import PersonFinderController
+
+class MockPerson():
+    def __init__(self, first_name, last_name, pet_name, pet_type) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet_name = pet_name
+        self.pet_type = pet_type
+
+class MockPeopleRepository:
+    def get_person(self, person_id: int):
+        return MockPerson(
+            first_name="Sandro",
+            last_name="Mazzolla",
+            pet_name="Thunder",
+            pet_type="Dog"
+        )
+
+def test_find():
+    controller = PersonFinderController(MockPeopleRepository())
+    response = controller.find_person(1)
+
+    expected_response = {
+        "data": {
+            "type": "Person",
+            "count": 1,
+            "attributes": {
+                "first_name": "Sandro",
+                "last_name": "Mazzolla",
+                "pet_name": "Thunder",
+                "pet_type": "Dog"
+            }
+        }
+    }
+
+    assert response == expected_response
